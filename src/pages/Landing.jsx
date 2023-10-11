fe2218ecc002cc8c9c349cb8941438b3586ac64b
Hubert Szkodzinski <hubertszkodzinski.it@gmail.com>
2023-10-11 20:04:49 +0200

    loader in landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,18 @@
+import { useLoaderData } from 'react-router-dom';
+import axios from 'axios';
+
+const cocktailSearchUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+export const loader = async () => {
+  const searchTerm = '';
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  return { drinks: response.data.drinks, searchTerm };
+};
+
 function Landing() {
+  const { drinks, searchTerm } = useLoaderData();
+  console.log(drinks);
   return <h2>Landing</h2>;
 }
 export default Landing;
